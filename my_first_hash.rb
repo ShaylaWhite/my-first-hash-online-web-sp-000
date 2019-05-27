8cedb49d644521cb489419eac2954caf08731741
Shayla White <shayladwhite1@gmail.com>
2019-05-27 22:13:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,9 +8,15 @@ end
 def shipping_manifest
   shipping_manifest = {
     
+<<<<<<< HEAD
 "whale bone corsets" => 5,
 "porcelain vases" => 2,
 "oil paintings" => 3,
+=======
+"whale bone corsets" => "5"
+"porcelain vases" => "2
+"oil paintings" => 3
+>>>>>>> 1cb4be39565cdb17bff2150c564f868b324febe4
     
   }
 
@@ -23,12 +29,18 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
                  }
+<<<<<<< HEAD
 
  shipping_manifest["oil paintings"]
+=======
+end
+ hash["oil paintings"]
+>>>>>>> 1cb4be39565cdb17bff2150c564f868b324febe4
 
 end
 
 def adding
+<<<<<<< HEAD
   shipping_manifest = {"whale bone corsets" => 5,
                        "porcelain vases" => 2,
                        "oil paintings" => 3,
@@ -36,6 +48,14 @@ def adding
 
 shipping_manifest["muskets"] =	2
 shipping_manifest["gun powder"] = 4
+=======
+  shipping_manifest = {
+"whale bone corsets" => 5
+"porcelain vases" => 2
+"oil paintings" => 3
+"muskets" => 2
+"gun powder" => 4 }
+>>>>>>> 1cb4be39565cdb17bff2150c564f868b324febe4
 
 shipping_manifest
 end
